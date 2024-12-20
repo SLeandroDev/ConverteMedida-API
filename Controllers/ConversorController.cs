036021adc107d063daf0e65644a0b21b222854a0
Leandro Silva <sleandrodevs@gmail.com>
2024-12-20 00:45:32 -0300

    teste

---
+++ Controllers/ConversorController.cs
@@ -0,0 +1,134 @@
+﻿using ConversorDeMedidas.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using static ConversorDeMedidas.API.Services.ConversorServiceComprimento;
+using static ConversorDeMedidas.API.Services.ConversorServiceLiquido;
+using static ConversorDeMedidas.API.Services.ConversorServicePeso;
+
+namespace ConversorDeMedidas.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/comprimento")]
+    public class ConversorComprimentoController : ControllerBase
+    {
+        private readonly ConversorServiceComprimento _conversorServiceComprimento;
+
+        public ConversorComprimentoController()
+        {
+            _conversorServiceComprimento = new ConversorServiceComprimento();
+        }
+
+        [HttpGet()]
+        public ActionResult<double> Convert(double valor, string unidadeOrigem, string unidadeDestino)
+        {
+            if (valor == 0)
+            {
+                return BadRequest("Favor preencha um valor para ser convertido.");
+            }
+
+            var mensagemErro = unidadeOrigem switch
+            {
+                "Selecione" => "Favor selecionar uma unidade de origem.",
+                _ when unidadeDestino == "Selecione" => "Favor selecionar uma unidade de destino.",
+                _ when unidadeOrigem == unidadeDestino => "Favor selecionar opções diferentes para conversão.",
+                _ => null
+            };
+
+            if (!string.IsNullOrEmpty(mensagemErro))
+            {
+                return BadRequest(mensagemErro);
+            }
+
+            if (!Enum.TryParse<UnidadeComprimento>(unidadeOrigem, true, out var origem) ||
+                !Enum.TryParse<UnidadeComprimento>(unidadeDestino, true, out var destino))
+            {
+                return BadRequest("Unidades de origem ou destino inválidas.");
+            }
+
+            var resultado = _conversorServiceComprimento.Converter(valor, origem, destino);
+            return Ok(resultado);
+        }
+    }
+
+    [Route("api/[controller]/peso")]
+    public class ConversorPesoController : ControllerBase
+    {
+        private readonly ConversorServicePeso _conversorServicePeso;
+
+        public ConversorPesoController()
+        {
+            _conversorServicePeso = new ConversorServicePeso();
+        }
+
+        [HttpGet()]
+        public ActionResult<double> Convert(double valor, string unidadeOrigem, string unidadeDestino)
+            {
+
+            if (valor == 0)
+            {
+                return BadRequest("Favor preencha um valor para ser convertido.");
+            }
+
+            var mensagemErro = unidadeOrigem switch
+            {
+                "Selecione" => "Favor selecionar uma unidade de origem.",
+                _ when unidadeDestino == "Selecione" => "Favor selecionar uma unidade de destino.",
+                _ when unidadeOrigem == unidadeDestino => "Favor selecionar opções diferentes para conversão.",
+                _ => null
+            };
+
+            if (!string.IsNullOrEmpty(mensagemErro))
+            {
+                return BadRequest(mensagemErro);
+            }
+
+            if (!Enum.TryParse<UnidadePeso>(unidadeOrigem, true, out var origem) ||
+                !Enum.TryParse<UnidadePeso>(unidadeDestino, true, out var destino))
+            {
+                return BadRequest("Unidades de origem ou destino inválidas.");
+            }
+            var resultado = _conversorServicePeso.Converter(valor, origem, destino);
+            return Ok(resultado);
+        }
+    }
+
+    [Route("api/[controller]/liquido")]
+    public class ConversorLiquidoController : ControllerBase
+    {
+        private readonly ConversorServiceLiquido _conversorServiceLiquido;
+
+        public ConversorLiquidoController()
+        {
+            _conversorServiceLiquido = new ConversorServiceLiquido();
+        }
+
+        [HttpGet()]
+        public ActionResult<double> Convert(double valor, string unidadeOrigem, string unidadeDestino)
+        {
+            if(valor == 0)
+{
+                return BadRequest("Favor preencha um valor para ser convertido.");
+            }
+
+            var mensagemErro = unidadeOrigem switch
+            {
+                "Selecione" => "Favor selecionar uma unidade de origem.",
+                _ when unidadeDestino == "Selecione" => "Favor selecionar uma unidade de destino.",
+                _ when unidadeOrigem == unidadeDestino => "Favor selecionar opções diferentes para conversão.",
+                _ => null
+            };
+
+            if (!string.IsNullOrEmpty(mensagemErro))
+            {
+                return BadRequest(mensagemErro);
+            }
+
+            if (!Enum.TryParse<UnidadeLiquido>(unidadeOrigem, true, out var origem) ||
+                !Enum.TryParse<UnidadeLiquido>(unidadeDestino, true, out var destino))
+            {
+                return BadRequest("Unidades de origem ou destino inválidas.");
+            }
+            var resultado = _conversorServiceLiquido.Converter(valor, origem, destino);
+            return Ok(resultado);
+        }
+    }
+}
---
+++ Services/ConversorService.cs
@@ -0,0 +1,103 @@
+﻿using static ConversorDeMedidas.API.Services.ConversorServiceComprimento;
+
+namespace ConversorDeMedidas.API.Services
+{
+    public class ConversorServiceComprimento
+    {
+        public enum UnidadeComprimento
+        {
+            Metros,
+            centimetros,
+            Polegadas
+        }
+        public double Converter(double valor, UnidadeComprimento unidadeOrigem, UnidadeComprimento unidadeDestino)
+        {
+            // Lógica de conversão ()
+
+            var conversao = new Dictionary<(UnidadeComprimento, UnidadeComprimento), double>
+            {
+                {(UnidadeComprimento.Metros, UnidadeComprimento.centimetros), 100},
+                {(UnidadeComprimento.centimetros, UnidadeComprimento.Metros), 0.01},
+                {(UnidadeComprimento.Polegadas, UnidadeComprimento.centimetros), 2.54},
+                {(UnidadeComprimento.centimetros, UnidadeComprimento.Polegadas), 1 / 2.54},
+                {(UnidadeComprimento.Polegadas, UnidadeComprimento.Metros), 0.0254},
+                {(UnidadeComprimento.Metros, UnidadeComprimento.Polegadas), 39.37}
+            };
+
+            if (conversao.TryGetValue((unidadeOrigem, unidadeDestino), out double taxa))
+            {
+                return valor * taxa;
+            }
+            else
+            {
+                throw new ArgumentException("Conversão não suportada para as unidades especificadas.");
+            }
+        }
+    }
+
+    public class ConversorServicePeso
+    {
+        public enum UnidadePeso
+        {
+            Kg,
+            Gramas,
+            Toneladas
+        }
+        public double Converter(double valor, UnidadePeso unidadeOrigem, UnidadePeso unidadeDestino)
+        {
+            // Lógica de conversão ()
+
+            var conversao = new Dictionary<(UnidadePeso, UnidadePeso), double>
+            {
+                {(UnidadePeso.Kg, UnidadePeso.Gramas), 1000},
+                {(UnidadePeso.Gramas, UnidadePeso.Kg), 0.001},
+                {(UnidadePeso.Kg, UnidadePeso.Toneladas), 0.001},
+                {(UnidadePeso.Toneladas, UnidadePeso.Kg), 1000},
+                {(UnidadePeso.Gramas, UnidadePeso.Toneladas), 0.000001},
+                {(UnidadePeso.Toneladas, UnidadePeso.Gramas), 1000000}
+            };
+
+            if (conversao.TryGetValue((unidadeOrigem, unidadeDestino), out double taxa))
+            {
+                return valor * taxa;
+            }
+            else
+            {
+                throw new ArgumentException("Conversão não suportada para as unidades especificadas.");
+            }
+        }
+    }
+
+    public class ConversorServiceLiquido
+    {
+        public enum UnidadeLiquido
+        {
+            Litros,
+            Mililitros,
+            MetrosCubicos
+        }
+
+        public double Converter(double valor, UnidadeLiquido unidadeOrigem, UnidadeLiquido unidadeDestino)
+        {
+            // Lógica de conversão
+            var conversao = new Dictionary<(UnidadeLiquido, UnidadeLiquido), double>
+            {
+                {(UnidadeLiquido.Litros, UnidadeLiquido.Mililitros), 1000},
+                {(UnidadeLiquido.Mililitros, UnidadeLiquido.Litros), 0.001},
+                {(UnidadeLiquido.Litros, UnidadeLiquido.MetrosCubicos), 0.001},
+                {(UnidadeLiquido.MetrosCubicos, UnidadeLiquido.Litros), 1000},
+                {(UnidadeLiquido.Mililitros, UnidadeLiquido.MetrosCubicos), 0.000001},
+                {(UnidadeLiquido.MetrosCubicos, UnidadeLiquido.Mililitros), 1000000}
+            };
+
+            if (conversao.TryGetValue((unidadeOrigem, unidadeDestino), out double taxa))
+            {
+                return valor * taxa;
+            }
+            else
+            {
+                throw new ArgumentException("Conversão não suportada para as unidades especificadas.");
+            }
+        }
+    }
+}
